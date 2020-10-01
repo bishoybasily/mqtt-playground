7bccb10fdd0f7b63b78be8dbf881e350f22749cc
bishoybasily <bishoybasily@gmail.com>
2020-10-01 23:33:00 +0200

    first commit

---
+++ src/test/java/com/gmail/bishoybasily/mqttplayground/MqttPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.bishoybasily.mqttplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
